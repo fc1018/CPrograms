126ff693be0fc2b6b1dee55e381b6371f398bfb2
Felix <mail@felixchan.net>
2021-07-25 22:34:38 +0100

    time structures

--- ch8/ch8_exercise1.5.c
+++ ch8/ch8_exercise1.5.c
@@ -14,11 +14,34 @@ int main (void) {
     struct time currenttime, nextTime;
 
     printf ("Enter the current time (hh:mm:ss): ");
-    scanf ("%i:%i:%i", &currentTime.hour, &currenttime.minutes, &currenttime.seconds);
+    scanf ("%i:%i:%i", &currenttime.hour, &currenttime.minutes, &currenttime.seconds);
 
     nextTime = timeUpdate (currenttime);
 
-    print ("Updated time is %.2i:%.2i:%.2i\n", nextTime.hour, nextTime.minutes, nextTime.seconds );
+    printf ("Updated time is %.2i:%.2i:%.2i\n", nextTime.hour, nextTime.minutes, nextTime.seconds );
 
     return 0;
+}
+
+//Function to update the time by one second 
+
+struct time timeUpdate (struct time now) {
+    now.seconds++;
+
+    if (now.seconds == 60) {
+        now.seconds = 0;
+        now.minutes++;
+
+        if (now.minutes == 60) {
+            now.minutes = 0;
+            now.hour++;
+
+            if ( now.hour == 24) {
+                now.hour = 0;
+            }
+        }
+    }
+
+
+    return now;
 }
