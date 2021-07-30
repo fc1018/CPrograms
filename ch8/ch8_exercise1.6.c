7fe3a106f6c931007fce9629243a2090590b073c
Felix <mail@felixchan.net>
2021-07-30 14:53:27 +0100

    arrays of structures

--- ch8/ch8_exercise1.6.c
+++ ch8/ch8_exercise1.6.c
@@ -13,7 +13,7 @@ int main (void) {
     struct time timeUpdate (struct time now);
     struct time testTimes[5] =
         { { 11, 59, 59 }, { 12, 0, 0 }, { 1, 29, 59 },
-          { 23, 59, 59 }, { 19, 12, 27 }};
+          { 23, 59, 59 }, { 19, 12, 27 } };
     
     for ( int i = 0; i < 5; i++ ) {
         printf ("Time is %.2i:%.2i:%.2i", testTimes[i].hour,
